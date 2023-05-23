ad622ff62a09048e1edab5a32174b02653493e5d
Juan Pablo Ugarte <juanpablougarte@gmail.com>
2023-05-23 09:53:34 -0400

    Tests: black update

--- tests/test_merengue_xml.py
+++ tests/test_merengue_xml.py
@@ -2,14 +2,14 @@
 Test project export xml for Merengue
 """
 import os
-from cambalache import CmbProject, config
+from cambalache import CmbProject
 from gi.repository import GObject, Gtk
 from lxml import etree
 
 
 def cmb_workspace_default_test(target_tk, type_class, ws_properties):
     """
-        Check type_class has ws_properties by default
+    Check type_class has ws_properties by default
     """
     project = CmbProject(target_tk=target_tk)
 
@@ -18,40 +18,40 @@ def cmb_workspace_default_test(target_tk, type_class, ws_properties):
 
     # Create object with workspace defaults
     obj = project.add_object(ui.ui_id, type_class)
-    assert(obj is not None)
+    assert obj is not None
 
     # Export UI file in merengue mode (Workspace)
     str_exported = project.db.tostring(ui.ui_id, merengue=True)
 
     # Check export worked
-    assert(str_exported is not None)
+    assert str_exported is not None
 
     # Parse results
     root = etree.fromstring(str_exported.encode())
 
     # Find object node
     obj_node = root.find(f"object[@class='{type_class}']")
-    assert(obj_node is not None)
+    assert obj_node is not None
 
     for property_id, value, exp in ws_properties:
         # Look for a property node with name==property_id
         prop = obj_node.find(f'property[@name="{property_id}"]')
-        assert(prop is not None)
+        assert prop is not None
 
         # Check value
         if value:
-            assert(prop.text==value)
+            assert prop.text == value
 
         # Check property xpath expression
         if exp:
-            assert(len(prop.xpath(exp)) > 0)
+            assert len(prop.xpath(exp)) > 0
 
 
 def test_object_id():
     """
-        Make sure merengue output has object ids in the form __cmb__{ui_id}.{object_id}
+    Make sure merengue output has object ids in the form __cmb__{ui_id}.{object_id}
     """
-    project = CmbProject(target_tk='gtk-4.0')
+    project = CmbProject(target_tk="gtk-4.0")
 
     # Add an UI
     ui = project.add_ui("test.ui")
@@ -60,37 +60,37 @@ def test_object_id():
 
     # GtkWindow
     win = project.add_object(ui.ui_id, "GtkWindow", "window")
-    assert(win is not None)
+    assert win is not None
     objects.append(win)
 
     # GtkBox
     box = project.add_object(ui.ui_id, "GtkBox", "box", parent_id=win.object_id)
-    assert(box is not None)
+    assert box is not None
     objects.append(box)
 
     # GtkButton
     button = project.add_object(ui.ui_id, "GtkButton", "button", parent_id=box.object_id)
-    assert(button is not None)
+    assert button is not None
     objects.append(button)
 
     # Export UI file in merengue mode (Workspace)
     str_exported = project.db.tostring(ui.ui_id, merengue=True)
 
     # Check export worked
-    assert(str_exported is not None)
+    assert str_exported is not None
 
     builder = Gtk.Builder()
     builder.add_from_string(str_exported)
 
     for obj in objects:
         gobject = builder.get_object(f"__cmb__{ui.ui_id}.{obj.object_id}")
-        assert(gobject is not None)
-        assert(GObject.type_name(type(gobject))==obj.type_id)
+        assert gobject is not None
+        assert GObject.type_name(type(gobject)) == obj.type_id
 
 
 def test_no_signals():
     """
-        Make sure merengue output does not have signals declaration to avoid GtkBuilder errors not finding the callbacks
+    Make sure merengue output does not have signals declaration to avoid GtkBuilder errors not finding the callbacks
     """
     path = os.path.join(os.path.dirname(__file__), "gtk+-3.0", "signals.ui")
 
@@ -100,11 +100,12 @@ def test_no_signals():
 
     root = etree.fromstring(str_exported.encode())
 
-    assert(root.find(f"signal") is None)
+    assert root.find("signal") is None
 
 
 def test_gtk4_stack_page_workspace_default():
-    cmb_workspace_default_test("gtk-4.0", "GtkStackPage",
-        [("child", None, "object[@class='GtkLabel']/property[@name='label' and text()='Empty Page']")]
+    cmb_workspace_default_test(
+        "gtk-4.0",
+        "GtkStackPage",
+        [("child", None, "object[@class='GtkLabel']/property[@name='label' and text()='Empty Page']")],
     )
-
--- tests/test_project_catalogs.py
+++ tests/test_project_catalogs.py
@@ -1,9 +1,8 @@
 """
 Test CmbObject API
 """
-import os
 
-from cambalache import CmbProject, config
+from cambalache import CmbProject
 
 
 def cmb_catalog_data_test(target_tk):
@@ -12,19 +11,21 @@ def cmb_catalog_data_test(target_tk):
 
     # Check for a known iface
     row = project.db.execute("SELECT parent_id FROM type WHERE type_id='GtkOrientable';").fetchall()
-    assert(row is not None)
-    assert(len(row) == 1)
-    assert(row[0][0] == "interface")
+    assert row is not None
+    assert len(row) == 1
+    assert row[0][0] == "interface"
 
     # Check a know iface property
-    row = project.db.execute("SELECT property_id FROM property WHERE owner_id='GtkOrientable' AND property_id='orientation';").fetchall()
-    assert(row is not None)
-    assert(len(row) == 1)
+    row = project.db.execute(
+        "SELECT property_id FROM property WHERE owner_id='GtkOrientable' AND property_id='orientation';"
+    ).fetchall()
+    assert row is not None
+    assert len(row) == 1
 
     # Check a type has the iface
     row = project.db.execute("SELECT iface_id FROM type_iface WHERE type_id='GtkBox' AND iface_id='GtkOrientable';").fetchall()
-    assert(row is not None)
-    assert(len(row) == 1)
+    assert row is not None
+    assert len(row) == 1
 
 
 def test_gtk3_project_catalogs_data():
