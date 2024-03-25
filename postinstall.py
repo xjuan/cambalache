7aeb9419e72a34b765aec60bf86fb497a702544d
Juan Pablo Ugarte <juanpablougarte@gmail.com>
2024-03-25 17:38:13 -0400

    meson: use gnome.post_install()

--- postinstall.py
+++ postinstall.py
@@ -1,27 +1,9 @@
 #!/usr/bin/env python3
 
 import compileall
-from os import environ, path
-from subprocess import call
+from os import environ
 
-prefix = environ.get("MESON_INSTALL_PREFIX", "/usr/local")
-datadir = path.join(prefix, "share")
 destdir = environ.get("DESTDIR", "")
-
-# Package managers set this so we don't need to run
-if not destdir:
-    print("Updating mime database...")
-    call(["update-mime-database", path.join(datadir, "mime")])
-
-    print("Updating icon cache...")
-    call(["gtk-update-icon-cache", "-qtf", path.join(datadir, "icons", "hicolor")])
-
-    print("Updating desktop database...")
-    call(["update-desktop-database", "-q", path.join(datadir, "applications")])
-
-    print("Compiling GSettings schemas...")
-    call(["glib-compile-schemas", path.join(datadir, "glib-2.0", "schemas")])
-
 
 # Pre compile all .py files
 compileall.compile_dir(destdir, force=True)
