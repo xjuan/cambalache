ff670f4081cedecfeaf065d81e884e0823afe7c6
Juan Pablo Ugarte <juanpablougarte@gmail.com>
2024-07-29 18:04:33 -0400

    CmbObjectChooser: add support for drop target
    Fix issue #219 "Move existing widgets / hierarchy sections into property fields"

--- cambalache/control/cmb_object_chooser.py
+++ cambalache/control/cmb_object_chooser.py
@@ -22,7 +22,7 @@
 #
 
 from cambalache import _
-from gi.repository import GObject, Gtk
+from gi.repository import GObject, Gdk, Gtk
 
 from ..cmb_object import CmbObject
 from ..cmb_project import CmbProject
@@ -35,7 +35,7 @@ class CmbObjectChooser(Gtk.Entry):
     project = GObject.Property(type=CmbProject, flags=GObject.ParamFlags.READWRITE | GObject.ParamFlags.CONSTRUCT_ONLY)
     parent = GObject.Property(type=CmbObject, flags=GObject.ParamFlags.READWRITE | GObject.ParamFlags.CONSTRUCT_ONLY)
     is_inline = GObject.Property(type=bool, default=False, flags=GObject.ParamFlags.READWRITE | GObject.ParamFlags.CONSTRUCT_ONLY)
-    inline_object_id = GObject.Property(type=str, default=None, flags=GObject.ParamFlags.READWRITE | GObject.ParamFlags.CONSTRUCT_ONLY)
+    inline_object_id = GObject.Property(type=str, default=None, flags=GObject.ParamFlags.READWRITE | GObject.ParamFlags.CONSTRUCT)
     inline_property_id = GObject.Property(type=str, default=None, flags=GObject.ParamFlags.READWRITE | GObject.ParamFlags.CONSTRUCT_ONLY)
     type_id = GObject.Property(type=str, default=None, flags=GObject.ParamFlags.READWRITE | GObject.ParamFlags.CONSTRUCT_ONLY)
 
@@ -51,10 +51,64 @@ class CmbObjectChooser(Gtk.Entry):
 
         if self.is_inline:
             self.connect("icon-press", self.__on_icon_pressed)
-            self.parent.connect("property-changed", lambda o, p: self.__update_icons())
+            self.parent.connect("property-changed", self.__on_parent_property_changed)
             self.__update_icons()
         else:
             self.props.placeholder_text = f"<{self.type_id}>"
+
+        drop_target = Gtk.DropTarget.new(
+            type=GObject.TYPE_NONE, actions=Gdk.DragAction.COPY
+        )
+        drop_target.set_gtypes([CmbObject])
+        drop_target.connect("accept", self.__on_drop_accept)
+        drop_target.connect("drop", self.__on_drop_drop)
+        self.add_controller(drop_target)
+
+    def __on_parent_property_changed(self, parent, prop):
+        if prop.property_id != self.inline_property_id:
+            return
+
+        self.inline_object_id = prop.inline_object_id
+        self.__update_icons()
+
+    def __on_drop_accept(self, target, drop):
+        drag = drop.get_drag()
+        origin_item = drag._item
+
+        if origin_item == self.parent:
+            return False
+
+        return origin_item.info.is_a(self.type_id)
+
+    def __on_drop_drop(self, target, origin_item, x, y):
+        if self.is_inline:
+            prop = self.parent.properties_dict.get(self.inline_property_id, None)
+
+            if prop is None:
+                return
+
+            self.parent.project.history_push(_("Move {name} to {property}").format(
+                name=origin_item.display_name,
+                property=f"{self.parent.type_id}::{self.inline_property_id}"))
+
+            # TODO: implement this in plain SQL
+            parent_id = self.parent.object_id
+            object_id = origin_item.object_id
+
+            origin_item.parent_id = parent_id
+            origin_item.inline_property_id = self.inline_property_id
+
+            prop.value = object_id
+
+            prop.inline_object_id = object_id
+            self.inline_object_id = object_id
+            self.__update_icons()
+
+            self.parent.project.history_pop()
+        else:
+            # TODO: ensure dragged object has an id
+            # Select dragged object id
+            self.cmb_value = origin_item.object_id
 
     def __on_text_notify(self, obj, pspec):
         if self.__updating or self.inline_object_id:
@@ -124,6 +178,7 @@ class CmbObjectChooser(Gtk.Entry):
         if self.inline_object_id:
             obj = project.get_object_by_id(self.parent.ui_id, self.inline_object_id)
             project.remove_object(obj)
+            self.inline_object_id = None
             self.__update_icons()
         else:
             chooser = CmbTypeChooserPopover(parent_type_id=parent.type_id, derived_type_id=self.type_id)
