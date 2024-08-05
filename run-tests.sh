84f34c0a49290e8ba7c91b56cf13f42758932918
Juan Pablo Ugarte <juanpablougarte@gmail.com>
2024-08-05 19:04:15 -0400

    run-test.sh update env vars

--- run-tests.sh
+++ run-tests.sh
@@ -2,8 +2,11 @@
 
 SCRIPT=$(readlink -f $0)
 DIRNAME=$(dirname $SCRIPT)
-export LD_LIBRARY_PATH=$DIRNAME/.local/lib/x86_64-linux-gnu
-export GI_TYPELIB_PATH=$DIRNAME/.local/lib/x86_64-linux-gnu/girepository-1.0
-export PKG_CONFIG_PATH=$DIRNAME/.local/lib/x86_64-linux-gnu/pkgconfig
+ARCH_TRIPLET=$(cc -dumpmachine)
+export LD_LIBRARY_PATH=$DIRNAME/.local/lib/$ARCH_TRIPLET:$LD_LIBRARY_PATH
+export GI_TYPELIB_PATH=$DIRNAME/.local/lib/$ARCH_TRIPLET/girepository-1.0:$GI_TYPELIB_PATH
+export PKG_CONFIG_PATH=$DIRNAME/.local/lib/$ARCH_TRIPLET/pkgconfig:$PKG_CONFIG_PATH
+export GSETTINGS_SCHEMA_DIR=$DIRNAME/.local/share/glib-2.0/schemas:$GSETTINGS_SCHEMA_DIR
+export XDG_DATA_DIRS=$DIRNAME/.local/share:$XDG_DATA_DIRS
 
 pytest $@
