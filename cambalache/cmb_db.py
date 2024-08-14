da2739ad12e534f7b557f28d7efd32e3ceca93f1
Juan Pablo Ugarte <juanpablougarte@gmail.com>
2024-08-14 08:36:43 -0400

    CmbDB: add db profile feature
    Use custom sqlite3 connection and cursor wrapper classes to log
    queries in __profile__ table.
    
    This is enabled with CAMBALACHE_DEBUG=db-profile env variable.

--- cambalache/cmb_db.py
+++ cambalache/cmb_db.py
@@ -25,6 +25,7 @@ import os
 import sys
 import sqlite3
 import ast
+import time
 
 from lxml import etree
 from lxml.builder import E
@@ -45,6 +46,101 @@ def _get_text_resource(name):
 BASE_SQL = _get_text_resource("db/cmb_base.sql")
 PROJECT_SQL = _get_text_resource("db/cmb_project.sql")
 HISTORY_SQL = _get_text_resource("db/cmb_history.sql")
+
+
+class CmbProfileConnection(sqlite3.Connection):
+    def __init__(self, path, **kwargs):
+        super().__init__(path, **kwargs)
+
+        self.executescript(
+            """
+            CREATE TABLE IF NOT EXISTS __profile__ (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                query TEXT NOT NULL,
+                plan TEXT,
+                executions INTEGER NOT NULL DEFAULT 1,
+                total_time INTEGER NOT NULL DEFAULT 0,
+                average_time INTEGER NOT NULL DEFAULT 0,
+                min_time INTEGER NOT NULL DEFAULT 0,
+                max_time INTEGER NOT NULL DEFAULT 0
+            );
+            """
+        )
+
+        # Striped querys PK dictionary
+        self._querys = {}
+
+        # Populate querys
+        for row in super().execute("SELECT id, query FROM __profile__;"):
+            id, query = row
+            self._querys[query] = id
+
+    def cursor(self):
+        return super(CmbProfileConnection, self).cursor(CmbProfileCursor)
+
+    def execute(self, *args):
+        start = time.monotonic_ns()
+        retval = super().execute(*args)
+        self.log_query(time.monotonic_ns() - start, *args)
+        return retval
+
+    def log_query(self, exec_time, *args):
+        query = args[0].strip()
+
+        if query.startswith("CREATE") or query.startswith("PRAGMA"):
+            return
+
+        # Convert from nano seconds to micro seconds
+        exec_time = int(exec_time / 1000)
+        pk_id = self._querys.get(query, None)
+
+        if pk_id is None:
+            # Get query plan
+            if len(args) > 1:
+                c = super().execute(f"EXPLAIN QUERY PLAN {query}", args[1])
+            else:
+                c = super().execute(f"EXPLAIN QUERY PLAN {query}")
+
+            # Convert plan to a string
+            plan = []
+            for row in c:
+                plan.append(" ".join(str(row)))
+            plan = "\n".join(plan)
+
+            # Create new query entry in profile table
+            c = super().execute(
+                """
+                INSERT INTO __profile__(query, plan, total_time, average_time, min_time, max_time)
+                VALUES(?, ?, ?, ?, ?, ?)
+                RETURNING id;
+                """,
+                (query, plan, exec_time, exec_time, exec_time, exec_time)
+            )
+            pk_id = c.fetchone()[0]
+            self._querys[query] = pk_id
+        else:
+            # Increment number of executions of this query
+            super().execute(
+                """
+                UPDATE __profile__
+                SET
+                    executions=executions+1,
+                    total_time=total_time+?,
+                    average_time=total_time/executions,
+                    min_time=min(min_time, ?),
+                    max_time=max(max_time, ?)
+                WHERE id=?;
+                """,
+                (exec_time, exec_time, exec_time, pk_id)
+            )
+
+
+class CmbProfileCursor(sqlite3.Cursor):
+    def execute(self, *args):
+        start = time.monotonic_ns()
+        retval = super().execute(*args)
+        self.connection.log_query(time.monotonic_ns() - start, *args)
+        return retval
 
 
 class CmbDB(GObject.GObject):
@@ -131,7 +227,11 @@ class CmbDB(GObject.GObject):
         self.conn.execute("PRAGMA quick_check;")
 
     def __sqlite_connect(self, path):
-        conn = sqlite3.connect(path)
+        debug_var = os.environ.get("CAMBALACHE_DEBUG", None)
+        if debug_var == "db-profile" :
+            conn = sqlite3.connect(path, factory=CmbProfileConnection)
+        else:
+            conn = sqlite3.connect(path)
 
         conn.create_collation("version", sqlite_version_cmp)
         conn.create_aggregate("MAX_VERSION", 1, MaxVersion)
