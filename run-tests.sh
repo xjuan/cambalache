2510f317c9aeb5dabe7ad451bb4ba862e6d772bb
Juan Pablo Ugarte <juanpablougarte@gmail.com>
2024-09-26 17:01:36 -0400

    run-tests.sh: set HOME to ensure empty recent list

--- run-tests.sh
+++ run-tests.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/bash
 
 source .local.env
+SCRIPT=$(readlink -f $0)
+DIRNAME=$(dirname $SCRIPT)
 export GSETTINGS_BACKEND=memory
+export HOME=$DIRNAME/.local/home
+mkdir -p $HOME/Projects
 
 pytest $@
