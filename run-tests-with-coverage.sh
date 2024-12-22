edee67cc72a6ff65359842312f2eca890b72d121
Juan Pablo Ugarte <juanpablougarte@gmail.com>
2024-12-22 12:34:46 -0500

    Move coverage script to root

---
+++ run-tests-with-coverage.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+source .local.env
+SCRIPT=$(readlink -f $0)
+DIRNAME=$(dirname $SCRIPT)
+export GSETTINGS_BACKEND=memory
+export HOME=$DIRNAME/.local/home
+mkdir -p $HOME/Projects
+
+export COVERAGE_PROCESS_START=$DIRNAME/pyproject.toml
+
+#Run tests with coverage if available
+if command -v python3-coverage &> /dev/null
+then
+  python3-coverage run --rcfile=$DIRNAME/pyproject.toml -m pytest $@
+  python3-coverage combine
+  python3-coverage report -m
+else
+  echo "python3-coverage not found;\n sudo apt install python3-coverage"
+fi
