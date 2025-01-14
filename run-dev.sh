8b02d13e014f343eadb8e511ace6ae085dedeb32
Juan Pablo Ugarte <juanpablougarte@gmail.com>
2025-01-13 19:20:14 -0500

    run-dev.sh: Bind translation domain
    This fixes running dev script in different languages.

--- run-dev.sh
+++ run-dev.sh
@@ -5,6 +5,9 @@ source .local.env
 if python3 $DIRNAME/tools/cmb_init_dev.py; then
     python3 - $@ << EOF
 import sys
+import locale
+locale.bindtextdomain("cambalache", "$DIRNAME/.local/share/locale")
+locale.textdomain("cambalache")
 from cambalache.app import CmbApplication
 CmbApplication().run(sys.argv)
 EOF
