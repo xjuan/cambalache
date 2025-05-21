6ac16ca8c0b4f8b8f7ba9fd6ad5f7710c6599b25
Juan Pablo Ugarte <juanpablougarte@gmail.com>
2025-05-21 19:01:53 -0400

    tests/test_import_export.py: Use parametrize

--- tests/test_import_export.py
+++ tests/test_import_export.py
@@ -4,11 +4,52 @@
 import .ui files into cambalache and export to compare results
 """
 import os
+import pytest
 
 from cambalache import CmbProject, config
 
 
-def assert_original_and_exported(target_tk, filename):
+@pytest.mark.parametrize("target_tk,filename", [
+    ("gtk+-3.0", "window.ui"),
+    ("gtk+-3.0", "children.ui"),
+    ("gtk+-3.0", "packing.ui"),
+    ("gtk+-3.0", "signals.ui"),
+    ("gtk+-3.0", "template.ui"),
+    ("gtk+-3.0", "comboboxtext.ui"),
+    ("gtk+-3.0", "dialog.ui"),
+    ("gtk+-3.0", "label.ui"),
+    ("gtk+-3.0", "levelbar.ui"),
+    ("gtk+-3.0", "liststore.ui"),
+    ("gtk+-3.0", "scale.ui"),
+    ("gtk+-3.0", "sizegroup.ui"),
+    ("gtk+-3.0", "style.ui"),
+    ("gtk+-3.0", "treestore.ui"),
+    ("gtk+-3.0", "filefilter.ui"),
+    ("gtk+-3.0", "custom_fragment.ui"),
+    ("gtk+-3.0", "bindings.ui"),
+    ("gtk+-3.0", "menu.ui"),
+    ("gtk+-3.0", "accessibility.ui"),
+    ("gtk-4.0", "window.ui"),
+    ("gtk-4.0", "children.ui"),
+    ("gtk-4.0", "layout.ui"),
+    ("gtk-4.0", "signals.ui"),
+    ("gtk-4.0", "template.ui"),
+    ("gtk-4.0", "inline_object.ui"),
+    ("gtk-4.0", "stack_page.ui"),
+    ("gtk-4.0", "liststore.ui"),
+    ("gtk-4.0", "treestore.ui"),
+    ("gtk-4.0", "comboboxtext.ui"),
+    ("gtk-4.0", "style.ui"),
+    ("gtk-4.0", "label.ui"),
+    ("gtk-4.0", "filefilter.ui"),
+    ("gtk-4.0", "custom_fragment.ui"),
+    ("gtk-4.0", "bindings.ui"),
+    ("gtk-4.0", "menu.ui"),
+    ("gtk-4.0", "string_list.ui"),
+    ("gtk-4.0", "accessibility.ui"),
+    ("gtk-4.0", "ui_comments.ui")
+])
+def test_(target_tk, filename):
     """
     import .ui file and compare it with the exported version
     """
@@ -29,160 +70,3 @@ def assert_original_and_exported(target_tk, filename):
 
     assert str_exported == str_original
 
-
-#
-# Gtk+ 3.0 Tests
-#
-def test_gtk3_window():
-    assert_original_and_exported("gtk+-3.0", "window.ui")
-
-
-def test_gtk3_children():
-    assert_original_and_exported("gtk+-3.0", "children.ui")
-
-
-def test_gtk3_packing():
-    assert_original_and_exported("gtk+-3.0", "packing.ui")
-
-
-def test_gtk3_signals():
-    assert_original_and_exported("gtk+-3.0", "signals.ui")
-
-
-def test_gtk3_template():
-    assert_original_and_exported("gtk+-3.0", "template.ui")
-
-
-def test_gtk3_comboboxtext():
-    assert_original_and_exported("gtk+-3.0", "comboboxtext.ui")
-
-
-def test_gtk3_dialog():
-    assert_original_and_exported("gtk+-3.0", "dialog.ui")
-
-
-def test_gtk3_label():
-    assert_original_and_exported("gtk+-3.0", "label.ui")
-
-
-def test_gtk3_levelbar():
-    assert_original_and_exported("gtk+-3.0", "levelbar.ui")
-
-
-def test_gtk3_liststore():
-    assert_original_and_exported("gtk+-3.0", "liststore.ui")
-
-
-def test_gtk3_scale():
-    assert_original_and_exported("gtk+-3.0", "scale.ui")
-
-
-def test_gtk3_sizegroup():
-    assert_original_and_exported("gtk+-3.0", "sizegroup.ui")
-
-
-def test_gtk3_style():
-    assert_original_and_exported("gtk+-3.0", "style.ui")
-
-
-def test_gtk3_treestore():
-    assert_original_and_exported("gtk+-3.0", "treestore.ui")
-
-
-def test_gtk3_filefilter():
-    assert_original_and_exported("gtk+-3.0", "filefilter.ui")
-
-
-def test_gtk3_custom_fragment():
-    assert_original_and_exported("gtk+-3.0", "custom_fragment.ui")
-
-
-def test_gtk3_bindings():
-    assert_original_and_exported("gtk+-3.0", "bindings.ui")
-
-
-def test_gtk3_menu():
-    assert_original_and_exported("gtk+-3.0", "menu.ui")
-
-
-def test_gtk3_accessibility():
-    assert_original_and_exported("gtk+-3.0", "accessibility.ui")
-
-
-#
-# Gtk 4.0 Tests
-#
-def test_gtk4_window():
-    assert_original_and_exported("gtk-4.0", "window.ui")
-
-
-def test_gtk4_children():
-    assert_original_and_exported("gtk-4.0", "children.ui")
-
-
-def test_gtk4_layout():
-    assert_original_and_exported("gtk-4.0", "layout.ui")
-
-
-def test_gtk4_signals():
-    assert_original_and_exported("gtk-4.0", "signals.ui")
-
-
-def test_gtk4_template():
-    assert_original_and_exported("gtk-4.0", "template.ui")
-
-
-def test_gtk4_inline_object():
-    assert_original_and_exported("gtk-4.0", "inline_object.ui")
-
-
-def test_gtk4_stack_page():
-    assert_original_and_exported("gtk-4.0", "stack_page.ui")
-
-
-def test_gtk4_liststore():
-    assert_original_and_exported("gtk-4.0", "liststore.ui")
-
-
-def test_gtk4_treestore():
-    assert_original_and_exported("gtk-4.0", "treestore.ui")
-
-
-def test_gtk4_comboboxtext():
-    assert_original_and_exported("gtk-4.0", "comboboxtext.ui")
-
-
-def test_gtk4_style():
-    assert_original_and_exported("gtk-4.0", "style.ui")
-
-
-def test_gtk4_label():
-    assert_original_and_exported("gtk-4.0", "label.ui")
-
-
-def test_gtk4_filefilter():
-    assert_original_and_exported("gtk-4.0", "filefilter.ui")
-
-
-def test_gtk4_custom_fragment():
-    assert_original_and_exported("gtk-4.0", "custom_fragment.ui")
-
-
-def test_gtk4_bindings():
-    assert_original_and_exported("gtk-4.0", "bindings.ui")
-
-
-def test_gtk4_menu():
-    assert_original_and_exported("gtk-4.0", "menu.ui")
-
-
-def test_gtk4_string_list():
-    assert_original_and_exported("gtk-4.0", "string_list.ui")
-
-
-def test_gtk4_accessibility():
-    assert_original_and_exported("gtk-4.0", "accessibility.ui")
-
-
-def test_gtk4_ui_comments():
-    assert_original_and_exported("gtk-4.0", "ui_comments.ui")
