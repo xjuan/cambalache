8f8cdb8b5388c034de26270b6edd5ecfe0d14000
Juan Pablo Ugarte <juanpablougarte@gmail.com>
2025-07-27 10:27:45 -0400

    Tests: add dummy backend URL to make sure notifications are disabled during tests.

--- tests/__init__.py
+++ tests/__init__.py
@@ -7,6 +7,7 @@ basedir = os.path.join(os.path.split(os.path.dirname(__file__))[0])
 homedir = os.path.join(basedir, ".local", "home")
 os.makedirs(os.path.join(homedir, "Projects"), exist_ok=True)
 
+os.environ["CMB_NOTIFICATION_URL"] = "https://dummybackend.local:2000"
 os.environ["GSETTINGS_BACKEND"] = "memory"
 os.environ["HOME"] = homedir
 
