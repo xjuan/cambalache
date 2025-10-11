127ff1c80d1afe9b1126e5e20299bd9e483a4d15
Juan Pablo Ugarte <juanpablougarte@gmail.com>
2025-10-11 09:36:58 -0400

    Update build system

--- cambalache/__init__.py
+++ cambalache/__init__.py
@@ -94,10 +94,12 @@ from .cmb_view import CmbView
 from .cmb_list_view import CmbListView
 from .cmb_notification import notification_center, CmbNotification, CmbNotificationCenter
 from .cmb_notification_list_view import CmbNotificationListView
+from .cmb_object_property_editor import CmbObjectPropertyEditor
 from .cmb_object_editor import CmbObjectEditor
 from .cmb_signal_editor import CmbSignalEditor
-from .cmb_ui_editor import CmbUIEditor
+from .cmb_ui_property_editor import CmbUIPropertyEditor
 from .cmb_ui_requires_editor import CmbUIRequiresEditor
+from .cmb_ui_editor import CmbUIEditor
 from .cmb_css_editor import CmbCSSEditor
 from .cmb_gresource_editor import CmbGResourceEditor
 from .cmb_fragment_editor import CmbFragmentEditor
