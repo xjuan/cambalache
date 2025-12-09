c68c27196ad3f26344297818e14489c3ee36d0bc
Juan Pablo Ugarte <juanpablougarte@gmail.com>
2025-12-09 18:20:36 -0500

    CmbLibraryInfo: Add support for disabling library
     - Add type_info dictionary
     - Load/unload project's type info on enabled change

--- cambalache/cmb_library_info.py
+++ cambalache/cmb_library_info.py
@@ -25,16 +25,35 @@
 
 from gi.repository import GObject
 from .cmb_base_objects import CmbBaseLibraryInfo
+from .cmb_type_info import CmbTypeInfo
+from cambalache import _, CmbObject
 
 
 class CmbLibraryInfo(CmbBaseLibraryInfo):
-    third_party = GObject.Property(type=bool, default=False, flags=GObject.ParamFlags.READWRITE)
-
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
 
+        # Type Information
+        self.type_info = {}
+
         self.object_types = self.__init_object_types()
         self.min_version = self.__init_min_version()
+
+        # Init type_info for this library
+        for row in self.project.db.execute(
+            "SELECT * FROM type WHERE parent_id IS NOT NULL AND library_id=? ORDER BY type_id;",
+            (self.library_id, )
+        ):
+            type_id = row[0]
+            info = CmbTypeInfo.from_row(self.project, *row)
+            self.type_info[type_id] = info
+
+        # Set parent back reference
+        for type_id, info in self.type_info.items():
+            info.parent = self.type_info.get(info.parent_id, None) or self.project.type_info.get(info.parent_id)
+
+    def __str__(self):
+        return f"CmbLibraryInfo<{self.library_id}-{self.version}>"
 
     def __init_object_types(self):
         prefix = self.prefix
@@ -55,3 +74,27 @@ class CmbLibraryInfo(CmbBaseLibraryInfo):
         ).fetchone()
 
         return row[0] if row is not None else None
+
+    @GObject.Property(type=bool, default=False)
+    def enabled(self):
+        return self.db_get("SELECT enabled FROM library WHERE (library_id) IS (?);", (self.library_id,))
+
+    @enabled.setter
+    def _set_enabled(self, value):
+        if self.enabled == value:
+            return
+
+        self.db_set("UPDATE library SET enabled=? WHERE (library_id) IS (?);", (self.library_id,), value)
+
+        # Load or Unload type infos from project
+        if value:
+            self.project.type_info.update(self.type_info)
+            for type_id, info in self.type_info.items():
+                self.project.emit("type-info-added", info)
+        else:
+            for type_id in self.type_info.keys():
+                info = self.project.type_info.pop(type_id)
+                self.project.emit("type-info-removed", info)
+
+        self.project._library_info_changed(self, "enabled")
+
