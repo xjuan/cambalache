d5883dbe50377c6adf84155d56f9d03eba963c0a
Juan Pablo Ugarte <juanpablougarte@gmail.com>
2026-01-15 19:32:52 -0500

    MrgAdwDialog: fix child selection

--- cambalache/merengue/mrg_adw/mrg_adw_dialog.py
+++ cambalache/merengue/mrg_adw/mrg_adw_dialog.py
@@ -22,7 +22,7 @@
 # SPDX-License-Identifier: LGPL-2.1-only
 #
 
-from gi.repository import GObject, Adw, Gtk, CambalachePrivate
+from gi.repository import GObject, Adw, CambalachePrivate
 from merengue.mrg_gtk import MrgGtkWidget, MrgSelection
 from merengue import MrgPlaceholder
 
@@ -34,53 +34,27 @@ class MrgAdwDialog(MrgGtkWidget):
         self.__headerbar_height = 0
         super().__init__(**kwargs)
 
+        self.selection = MrgSelection(app=self.app, container=self.object)
+
     def object_changed(self, old, new):
         if old:
             old.close()
 
+        # Handle widget selection
+        if self.selection:
+            self.selection.container = self.object
+
         self.on_selected_changed()
 
         if self.object is None:
-            if self.window:
-                self.window.hide()
             return
 
-        if self.window is None:
-            self.window = self.__window_new()
-            self.selection = MrgSelection(app=self.app, container=self.window)
+        # Make sure we call adw_dialog_present()
+        self.object.present(None)
 
-        # Make sure we call adw_dialog_present() instead of adding the widget to a window
-        self.object.present(self.window)
-        self.window.present()
-
-        minimun, natural = self.object.get_preferred_size()
-        h_minimun, h_natural = self.__headerbar.get_preferred_size()
-
-        self.window.set_default_size(natural.width + 64, natural.height + h_natural.height * 2)
-
-        self.window.set_title(GObject.type_name(self.object.__gtype__))
-        CambalachePrivate.widget_set_application_id(self.window, f"Casilda:{self.ui_id}.{self.object_id}")
+        self.object.set_title(GObject.type_name(self.object.__gtype__))
+        CambalachePrivate.widget_set_application_id(self.object.props.parent, f"Casilda:{self.ui_id}.{self.object_id}")
         self.__update_placeholder()
-
-    def __window_new(self):
-        box = Gtk.Box(orientation=Gtk.Orientation.VERTICAL)
-        headerbar = Gtk.HeaderBar(valign=Gtk.Align.START, vexpand=False)
-
-        self.__headerbar = headerbar
-
-        button = Gtk.Button(label="Open", valign=Gtk.Align.CENTER, halign=Gtk.Align.CENTER, vexpand=True)
-        button.connect("clicked", self.__on_open_button_clicked)
-
-        label = Gtk.Label(label="This window is created automatically to be able to present the AdwDialog.", vexpand=True)
-
-        headerbar.pack_end(button)
-        box.append(headerbar)
-        box.append(label)
-        return Adw.Window(deletable=False, content=box)
-
-    def __on_open_button_clicked(self, button):
-        if self.object:
-            self.object.present(self.window)
 
     def __update_placeholder(self):
         if self.object is None:
