52a0a76a27bf34ca7a45028b7396c0884f3ddfa2
Matthieu Lorier <loriermatthieu@gmail.com>
2026-02-01 22:43:14 +0100

    Improve donate UI design

---
+++ cambalache/app/cmb_donate_dialog.py
@@ -0,0 +1,36 @@
+#
+# CmbDonateDialog
+#
+# Copyright (C) 2026  Matthieu Lorier
+#
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation;
+# version 2.1 of the License.
+#
+# library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+#
+# Authors:
+#   Matthieu Lorier <loriermatthieu@gmail.com>
+#
+# SPDX-License-Identifier: LGPL-2.1-only
+#
+
+from gi.repository import Gtk, Adw
+
+
+@Gtk.Template(resource_path="/ar/xjuan/Cambalache/app/cmb_donate_dialog.ui")
+class CmbDonateDialog(Adw.Dialog):
+    __gtype_name__ = "CmbDonateDialog"
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+Gtk.WidgetClass.set_css_name(CmbDonateDialog, "CmbDonateDialog")
