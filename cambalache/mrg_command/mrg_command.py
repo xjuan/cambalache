d6b5f1dc44fdc8f7c3b1d5f03208fdf3492f57e1
Juan Pablo Ugarte <juanpablougarte@gmail.com>
2026-05-29 14:56:07 -0400

    MrgCommand: write size of command instead of using new line as command delimiter.
     - Keep a reference of outgoing command data until finish callback is called
     - Rework implementation using read_bytes_async() to read an integer
       to know the length of the command.
    
    Fix issue #324 "Cambalache 1.0 fails to open large .ui files"

--- cambalache/mrg_command/mrg_command.py
+++ cambalache/mrg_command/mrg_command.py
@@ -13,8 +13,18 @@ class MrgCommand():
         self.__gsocket = None
         self.__conn = None
         self.__data_input = None
+
+        # List of outgoing commands
         self.__commands = []
-        self.__writing_command = False
+
+        # Current outgoing command buffer
+        self.__outgoing_data = None
+
+        # Incoming command message length
+        self.__incoming_total = 0
+
+        # Incoming buffer
+        self.__incoming_data = None
 
         if command_socket:
             self.init_connection(command_socket)
@@ -28,16 +38,21 @@ class MrgCommand():
             self.close_connection()
             return
 
-        self.__writing_command = False
+        self.__outgoing_data = None
         self.__consume_next_command()
 
     def __consume_next_command(self):
-        if self.__conn is None or self.__writing_command or not self.__commands:
+        if self.__conn is None or self.__outgoing_data or not self.__commands:
             return
 
-        self.__writing_command = True
+        command = self.__commands.pop(0)
+        command_len = len(command)
+
+        # Create command buffer by appending size to the beginning
+        self.__outgoing_data = command_len.to_bytes(4) + command
+
         self.__conn.props.output_stream.write_all_async(
-            self.__commands.pop(0),
+            self.__outgoing_data,
             0,
             self.__cancellable,
             self.__on_data_output
@@ -49,7 +64,7 @@ class MrgCommand():
         if args is not None:
             cmd["args"] = args
 
-        cmd = json.dumps(cmd) + "\n"
+        cmd = json.dumps(cmd)
 
         self.__commands.append(cmd.encode())
         self.__consume_next_command()
@@ -59,18 +74,52 @@ class MrgCommand():
 
     def __on_data_input(self, source, res):
         try:
-            line, len = source.read_line_finish_utf8(res)
+            data = source.read_bytes_finish(res)
         except Exception as e:
             if e.code != Gio.IOErrorEnum.CANCELLED:
                 logger.warning(f"Error reading command {e}")
             self.close_connection()
             return
 
-        if line is not None and line != "":
-            self.handle_command(line)
+        if self.__incoming_data:
+            self.__incoming_data = self.__incoming_data + data.get_data()
+        else:
+            self.__incoming_data = data.get_data()
+
+        # Check if we are done reading data for command
+        if data.get_size() != self.__incoming_total:
+            self.__incoming_total -= data.get_size()
+
+            # Read the rest
+            source.read_bytes_async(self.__incoming_total, 0, self.__cancellable, self.__on_data_input)
+            return
+
+        if self.__incoming_data is not None:
+            self.handle_command(self.__incoming_data.decode("UTF-8"))
+            self.__incoming_total = 0
+            self.__incoming_data = None
 
         # Queue next command read
-        source.read_line_async(0, self.__cancellable, self.__on_data_input)
+        source.read_bytes_async(4, 0, self.__cancellable, self.__on_data_len_input)
+
+    def __on_data_len_input(self, source, res):
+        try:
+            data = source.read_bytes_finish(res)
+        except Exception as e:
+            if e.code != Gio.IOErrorEnum.CANCELLED:
+                logger.warning(f"Error reading command {e}")
+            self.close_connection()
+            return
+
+        # Just in case
+        if data.get_size() < 4:
+            missing_data = source.read_bytes(4 - data.get_size(), None)
+            self.__incoming_total = int.from_bytes(data.get_data() + missing_data.get_data())
+        else:
+            self.__incoming_total = int.from_bytes(data.get_data())
+
+        # Read command data
+        source.read_bytes_async(self.__incoming_total, 0, self.__cancellable, self.__on_data_input)
 
     def init_connection(self, fd):
         self.__cancellable = Gio.Cancellable()
@@ -80,7 +129,7 @@ class MrgCommand():
         self.__data_input = Gio.DataInputStream.new(self.__conn.props.input_stream)
 
         # Queue first command read
-        self.__data_input.read_line_async(0, self.__cancellable, self.__on_data_input)
+        self.__data_input.read_bytes_async(4, 0, self.__cancellable, self.__on_data_len_input)
 
     def close_connection(self):
         # Cancel all pending IO
@@ -95,4 +144,6 @@ class MrgCommand():
             self.__gsocket = None
 
         self.__commands = []
-        self.__writing_command = False
+        self.__outgoing_data = None
+        self.__incoming_total = 0
+        self.__incoming_data = None
